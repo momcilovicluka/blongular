208cec7e84298aa787ee4109d820a173fdec361e
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-18 21:47:04 +0200

    feat: add comment list

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 import { CommentFormComponent } from '../../comments/comment-form/comment-form.component';
+import { CommentListComponent } from '../../comments/comment-list/comment-list.component';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [PostCardComponent, CommentFormComponent],
+  imports: [PostCardComponent, CommentFormComponent, CommentListComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
