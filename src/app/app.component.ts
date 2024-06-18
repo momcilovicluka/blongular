881644de288f95815bcae76a9c6420a59792de67
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-18 20:31:13 +0200

    feat: add subscription-form to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CategoryNavbarComponent, PostCardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CategoryNavbarComponent, PostCardComponent, SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
