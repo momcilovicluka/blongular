03d16eadd6741d419cb29120cc1047f76662c639
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-18 21:38:35 +0200

    feat: add single post

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { CommentFormComponent } from '../../comments/comment-form/comment-form.component';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent, CommentFormComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
