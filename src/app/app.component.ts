82c62bfe47120f5dfb173134ee0a7f4bed56cb47
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-18 19:45:22 +0200

    feat: add footer to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CategoryNavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'blongular';
