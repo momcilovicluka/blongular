9a64911eac3b615876551ea44bd7c15ca3f554d0
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 23:02:02 +0200

    feat: make featured dynamic

--- blongular/src/app/layouts/post-card/post-card.component.ts
+++ blongular/src/app/layouts/post-card/post-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
