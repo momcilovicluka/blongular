d416f734ac439da0f44f19852f4fa3a0ddb2c217
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 22:32:27 +0200

    feat: update home to dynamic

--- blongular/src/app/pages/home/home.component.ts
+++ blongular/src/app/pages/home/home.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../model/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private postService: PostsService) { }
 
+  featuredPostsArray: Array<Post> = [];
+  latestPostsArray: Array<Post> = [];
+
+  ngOnInit(): void {
+		this.postService.loadFeatured().subscribe((data) => {
+			this.featuredPostsArray = data;
+		});
+    
+    this.postService.loadAllOrderedByDate().subscribe((data) => {
+      this.latestPostsArray = data;
+    });
+	}
 }
