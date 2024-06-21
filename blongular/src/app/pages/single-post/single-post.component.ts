7536538da9109f2d2b3d5fe0b2234d092bcf8ccb
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 23:02:54 +0200

    feat: make single post dynamic

--- blongular/src/app/pages/single-post/single-post.component.ts
+++ blongular/src/app/pages/single-post/single-post.component.ts
@@ -2,14 +2,42 @@ import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 import { CommentFormComponent } from '../../comments/comment-form/comment-form.component';
 import { CommentListComponent } from '../../comments/comment-list/comment-list.component';
+import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../model/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [PostCardComponent, CommentFormComponent, CommentListComponent],
+  imports: [PostCardComponent, CommentFormComponent, CommentListComponent, CommonModule],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
 export class SinglePostComponent {
+  postData!: Post;
+  similarPostsArray: Array<Post> = [];
 
+  constructor(private route: ActivatedRoute, private postService: PostsService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.postService.loadOne(params['id']).subscribe((data) => {
+        this.postData = data;
+        this.loadSimilarPosts(this.postData.category.categoryId);
+      });
+
+      this.postService.countViews(params['id']);
+    });
+  }
+
+  convertTimestampToDate(timestamp: any): string | number | Date {
+		return new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
+	}
+
+  loadSimilarPosts(categoryId: string): void {
+    this.postService.loadSimilar(categoryId).subscribe((data) => {
+      this.similarPostsArray = data;
+    });
+  }
 }
