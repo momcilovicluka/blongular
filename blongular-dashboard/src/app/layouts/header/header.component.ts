8fc63e698c901707035039f61d98924c7c2dd060
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 20:58:10 +0200

    feat: add routing to header

--- blongular-dashboard/src/app/layouts/header/header.component.ts
+++ blongular-dashboard/src/app/layouts/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -11,12 +15,31 @@ export class HeaderComponent {
 
   userEmail: string = '';
 
+  router = inject(Router);
+
+  isLoggedIn$!: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+      // detect route changes
+      this.router.events.subscribe(() => {
+        this.ngOnInit();
+      });
+   }
+
   ngOnInit(): void {
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+    
     const user = localStorage.getItem('user');
     if (user !== null) {
       this.userEmail = JSON.parse(user).email;
     } else {
       this.userEmail = '';
     }
+    
+  }
+
+  onLogOut() {
+    this.authService.logOut();
+    this.ngOnInit();
   }
 }
