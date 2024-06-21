73b4c6d27fc8bdeb53e11d6929f697a63a6bcdab
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 22:31:50 +0200

    feat: add dynamic data to post card

--- blongular/src/app/layouts/post-card/post-card.component.ts
+++ blongular/src/app/layouts/post-card/post-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
 export class PostCardComponent {
 
+  @Input() postData: any;
+
+  ngOnInit(): void {
+    console.log(this.postData);
+  }
 }
