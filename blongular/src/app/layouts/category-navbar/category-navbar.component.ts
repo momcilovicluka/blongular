5dc1aa72b4e4382e4ebb9823aeb78c7961c471cf
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 22:32:10 +0200

    feat: add dynamic data to category navbar

--- blongular/src/app/layouts/category-navbar/category-navbar.component.ts
+++ blongular/src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
+import { Category } from '../../model/category';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './category-navbar.component.html',
   styleUrl: './category-navbar.component.css'
 })
 export class CategoryNavbarComponent {
+  categoryArray: Array<Category> = [];
 
+  constructor(private categoriesService: CategoriesService) {
+    this.categoriesService.loadData().subscribe(data => {
+      this.categoryArray = data;
+    });
+  }
 }
