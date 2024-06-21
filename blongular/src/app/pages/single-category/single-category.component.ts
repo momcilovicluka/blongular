566f1cd93d5811dbabb75915fc3abfd1fdbb7bc7
Luka Momčilović <l.momcilovic61@gmail.com>
2024-06-21 22:32:39 +0200

    feat: add dynamic datat to single category component

--- blongular/src/app/pages/single-category/single-category.component.ts
+++ blongular/src/app/pages/single-category/single-category.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../model/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-category',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommonModule],
   templateUrl: './single-category.component.html',
   styleUrl: './single-category.component.css'
 })
 export class SingleCategoryComponent {
+  constructor(private route: ActivatedRoute, private postService: PostsService) { }
 
+  PostsArray: Array<Post> = [];
+  categoryObject: any;
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.categoryObject = params;
+
+      this.postService.loadCategoryPosts(params['id']).subscribe((data) => {
+        this.PostsArray = data;
+      });
+  });
+  }
 }
